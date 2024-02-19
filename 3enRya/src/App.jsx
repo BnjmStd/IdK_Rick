0d1ad8356df2e99e542e9ff8c02e0c055c2831dd
bnjmn <benja_e_e@hotmail.com>
2024-02-19 14:59:44 -0300

    localstorageee

--- 3enRya/src/App.jsx
+++ 3enRya/src/App.jsx
@@ -15,7 +15,7 @@ const Square = ({ children, isSelected, updateBoard, index}) => {
 
 
   const handleClick = () => {
-    updateBoard()
+    updateBoard(index)
   };
 
   return (
@@ -29,15 +29,78 @@ const Square = ({ children, isSelected, updateBoard, index}) => {
   )
 };
 
+const WINNER_COMBOS = [
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+
 function App() {
   
   const [board, setBoard] = useState(Array(9).fill(null));
   
   const [turn, setTurn] = useState(TURNS.X);
 
-  const updateBoard = () => {
+  const [winner, setWinner] = useState(null);
+
+  const checkWinner = (boardToCheck) => {
+    for ( const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo;
+      if (
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+          return boardToCheck[a];
+      }
+
+    };
+    return null;
+  };
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null));
+    setTurn(TURNS.X);
+    setWinner(null);
+  
+  };
+
+  const checkEndGame = (newBoard) => {
+    return newBoard.every((Square) => Square !== null);
+  };
+
+
+  const updateBoard = (index) => {
+    /* No actualizar si ya tiene algo */
+    if (board[index] || winner) return 
+
+
+    /* actualizar el tablero */
+    const newBoard = [... board];
+    newBoard[index] = turn;
+    setBoard(newBoard);
+    
+    
+    /* cambiar el turno */
     const newTurn = turn === TURNS.X ?  TURNS.O : TURNS.X
     setTurn(newTurn);
+
+
+    /* revisar ganador */
+    const newWinner = checkWinner(newBoard);
+    if (newWinner) {
+      setWinner(newWinner);
+    } else if (checkEndGame(newBoard)){
+      setWinner(false);
+    };
+
+
+
   };
 
   return (
@@ -54,7 +117,7 @@ function App() {
                 updateBoard={updateBoard}
 
               >
-                {board[index]}
+                {_}
               </Square>
             ) 
           })
@@ -69,6 +132,30 @@ function App() {
           {TURNS.O}
         </Square>
       </section>
+
+      {
+        winner !== null && (
+          <section className='winner'>
+            <div className='text'> 
+            
+              <h2>
+                {
+                  winner === false 
+                  ? 'empate'
+                  : 'Ganó:' 
+                }
+              </h2>
+              <header className='win'>
+                {winner && <Square>{winner}</Square>}
+              </header>
+              <footer>
+                <button onClick={resetGame}>Empezar de nuevo</button>
+              </footer>
+            </div>
+          </section>
+        )
+      }
+
     </main>
   )
 }
